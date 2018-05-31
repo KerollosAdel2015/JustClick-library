c12edf9f1cb45c1c95b440f916c3357b13d4f264
Kerollos Adel (El-Kok) <37217653+KerollosAdel2015@users.noreply.github.com>
2018-05-31 02:05:32 +0200

    Add files via upload

---
+++ JustClick-Templete/JustClick Lib v1.1/js/JustClick-Templete - Copy.js
@@ -0,0 +1,173 @@
+var justclick = {};
+ 
+var version = "1 v"
+
+justclick.DateFormate = "DD-mm-YYYY"; 
+
+var indexArrayKeyWord = "{{@@index}}"
+var sequanceArrayKeyWord = "{{@@seq}}"
+
+var dateKeyWord = "{{@@date}}"
+var dateDayKeyWord = "{{@@day}}"
+var dateMonthKeyWord = "{{@@month}}"
+var dateYearKeyWord = "{{@@year}}"
+var dateMonthNameKeyWord = "{{@@month-name}}"
+var dateHourshKeyWord = "{{@@hours}}"
+var dateMinuteKeyWord = "{{@@minutes}}"
+var dateSecondKeyWord = "{{@@seconds}}"
+
+var versionKeyWord = "{{@@version}}"
+
+var urlKeyWord = "{{@@url}}" 
+
+var templeteDateFormate;
+
+
+//--------------------------------------------- start added function ----------------------------------------//
+String.prototype.replaceAll = function (searchText, replacementText) {
+    return this.split(searchText).join(replacementText);
+};
+//--------------------------------------------- end added function ----------------------------------------//
+
+// compile template by replace property name
+function ComplieSimple(_templete, _object) {
+    var pattern; 
+
+    for (var propertyName in _object) { 
+        _templete = _templete.replaceAll("{{" + propertyName + "}}", _object[propertyName]);
+    } 
+    return _templete;
+}
+
+function ComplieArray(_templete, _object) {
+    var templete = "";
+
+    for (var index = 0; index < _object.length; index++) { 
+        templete = templete + ComplieSimple(_templete, _object[index]);
+        templete = templete.replace(indexArrayKeyWord, index);
+        templete = templete.replace(sequanceArrayKeyWord, index + 1);
+    }
+
+    return templete;
+}
+
+// pattern {{#each porpName}}
+// get propName form the pattern
+function getPropName(single_proccess) {
+    var propName;
+
+    // get {{#each <<propName>>}} 
+    propName = single_proccess.substring(single_proccess.indexOf("{{#each"), single_proccess.indexOf("}}") + 2);
+
+    // remove {{#each from string
+    propName = propName.replace("{{#each", "");
+
+    // remove close each }}
+    propName = propName.replace("}}", "");
+
+    // remove any space
+    propName = propName.trim();
+
+    // just prop name
+    return propName;
+}
+
+// remove {{#each porpName}} & {{#end-each}}
+function clearForEachTemplete(single_proccess) {
+    var keyWord = single_proccess.substring(single_proccess.indexOf("{{#each"), single_proccess.indexOf("}}") + 2);
+     
+    // remove {{#each porpName}}
+    single_proccess = single_proccess.replace(keyWord, "");
+
+    // remove {{#end-each}}
+    single_proccess = single_proccess.replace("{{#end-each}}", "");
+
+    return single_proccess;
+}
+
+function handleDateTime(content_after_proccess) {
+    var _now = new Date();
+    var day = _now.getDate();
+    var month = _now.getMonth() + 1;
+    var monthName = _now.toLocaleString("en-us", { month: "long" });
+    var year = _now.getFullYear();
+    var hours = _now.getHours(); // => 9
+    var minutes =  _now.getMinutes(); // =>  30
+    var seconds = _now.getSeconds(); // => 51
+     
+    
+    // incase of there are date format att use it
+    if (templeteDateFormate == null || templeteDateFormate == ""){
+        content_after_proccess = content_after_proccess.replace(dateKeyWord, moment().format(justclick.DateFormate));
+    } else {
+        content_after_proccess = content_after_proccess.replace(dateKeyWord, moment().format(templeteDateFormate));
+    }
+     
+    content_after_proccess = content_after_proccess.replace(dateDayKeyWord, day);
+    content_after_proccess = content_after_proccess.replace(dateMonthKeyWord, month);
+    content_after_proccess = content_after_proccess.replace(dateYearKeyWord, year);
+    content_after_proccess = content_after_proccess.replace(dateMonthNameKeyWord, monthName);
+
+    content_after_proccess = content_after_proccess.replace(dateHourshKeyWord, hours);
+    content_after_proccess = content_after_proccess.replace(dateMinuteKeyWord, minutes);
+    content_after_proccess = content_after_proccess.replace(dateSecondKeyWord, seconds);
+    
+    return content_after_proccess;
+}
+ 
+justclick.Complie  = function (_templeteName , _targetContainerName, _object) {
+     
+    templeteDateFormate = $('#' + _templeteName).attr('jc-date-formate');
+     
+    var _templete = $("#" + _templeteName).html();
+
+    // get loop count
+    var loopCount = _templete.split("#each").length - 1; 
+    var content_after_proccess ="" ;
+    var single_proccess;
+    var propName;
+
+    for (var index = 0; index < loopCount; index++) {
+        // get part before each
+        single_proccess = _templete.substring(0, _templete.indexOf("{{#each"));
+        content_after_proccess = content_after_proccess + ComplieSimple(single_proccess, _object);
+
+
+        // get each loop content
+        single_proccess = _templete.substring(_templete.indexOf("{{#each"), _templete.indexOf("{{#end-each}}") + 13); 
+        propName = getPropName(single_proccess); 
+        single_proccess = clearForEachTemplete(single_proccess);
+
+        if (propName) { 
+            content_after_proccess = content_after_proccess + ComplieArray(single_proccess, _object[propName]);
+        } else { 
+            content_after_proccess = content_after_proccess + ComplieArray(single_proccess, _object);
+        }
+
+        
+        
+        // remove from string
+        _templete = _templete.substr(_templete.indexOf("{{#end-each}}") + 13);
+    }
+
+    if (_templete.length != 0) { 
+        content_after_proccess = content_after_proccess + ComplieSimple(_templete, _object);
+    }
+    
+    // if there are @@index , @@seq not in array
+    content_after_proccess = content_after_proccess.replace(indexArrayKeyWord, "");
+    content_after_proccess = content_after_proccess.replace(sequanceArrayKeyWord, "");
+    content_after_proccess = content_after_proccess.replace(urlKeyWord, window.location.href);
+    content_after_proccess = content_after_proccess.replace(versionKeyWord, version);
+      
+    // @@date
+    content_after_proccess = handleDateTime(content_after_proccess);
+     
+    $("#" + _targetContainerName).html(content_after_proccess);
+}
+
+justclick.ComplieFromURL = function (_templeteName, _targetContainerName, _serviceURL, params) {
+    $.getJSON(_serviceURL,params, function (_object) {
+        justclick.Complie(_templeteName, _targetContainerName, _object); 
+    });
+} 
---
+++ JustClick-Templete/JustClick Lib v1.1/src/Objects.js
@@ -0,0 +1,39 @@
+var task1 = {
+    "title": "Complete the peer assessment",
+    "due_by": "2 week from now",
+    "owner": "Kerollos Adel"
+};
+
+var task2 = {
+    "title": "Complete the peer assessment",
+    "due_by": "1 week from now",
+    "owner": "Amir Ali"
+};
+
+var task3 = {
+    "title": "Complete the peer assessment",
+    "due_by": "1.5 week from now",
+    "owner": "Samer Hassin"
+};
+
+var cars = [
+        { "name": "Jim's Ford Focus", "color": "white", "isDamaged": true, "wheels": 4 },
+        { "name": "Bob's Suzuki Swift", "color": "green", "isDamaged": false, "wheels": 4 },
+        { "name": "Alex's Harley Davidson", "color": "black", "isDamaged": false, "wheels": 2 }
+];
+
+var animals_data = {
+    CreatorName: "Kerollos Adel",
+    "animals": [
+{
+    image1: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Coast_Garter_Snake.jpg/500px-Coast_Garter_Snake.jpg",
+    image2: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Nerodia_sipedon_shedding.JPG/440px-Nerodia_sipedon_shedding.JPG",
+    name: "Snake"
+},
+{
+    image1: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Crocodilia_collage.jpg/600px-Crocodilia_collage.jpg",
+    image2: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Caiman_crocodilus_Tropicario_2.JPG/440px-Caiman_crocodilus_Tropicario_2.JPG",
+    name: "Crocodilia"
+}
+    ]
+};
